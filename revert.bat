e30d6fa5b1e09e93feffe67432613c6e735809e6
Cris <cbureriu@gmail.com>
2019-07-28 13:52:57 -0700

    Add files via upload

---
+++ revert.bat
@@ -0,0 +1,11 @@
+copy 14.7\ISE_DS\.xinstall\bin\nt\libPortability.dll.bak 14.7\ISE_DS\.xinstall\bin\nt\libPortability.dll
+copy 14.7\ISE_DS\common\lib\nt\libPortability.dll.bak 14.7\ISE_DS\common\lib\nt\libPortability.dll
+copy 14.7\ISE_DS\EDK\lib\nt\libPortability.dll.bak 14.7\ISE_DS\EDK\lib\nt\libPortability.dll
+copy 14.7\ISE_DS\ISE\lib\nt\libPortability.dll.bak 14.7\ISE_DS\ISE\lib\nt\libPortability.dll
+copy 14.7\ISE_DS\ISE\sysgen\bin\nt\libPortability.dll.bak 14.7\ISE_DS\ISE\sysgen\bin\nt\libPortability.dll
+
+copy 14.7\ISE_DS\.xinstall\bin\nt64\libPortability.dll.bak 14.7\ISE_DS\.xinstall\bin\nt64\libPortability.dll
+copy 14.7\ISE_DS\common\lib\nt64\libPortability.dll.bak 14.7\ISE_DS\common\lib\nt64\libPortability.dll
+copy 14.7\ISE_DS\EDK\lib\nt64\libPortability.dll.bak 14.7\ISE_DS\EDK\lib\nt64\libPortability.dll
+copy 14.7\ISE_DS\EDK\lib\nt64\sdk\libPortability.dll.bak 14.7\ISE_DS\EDK\lib\nt64\sdk\libPortability.dll
+copy 14.7\ISE_DS\ISE\sysgen\bin\nt64\libPortability.dll.bak 14.7\ISE_DS\ISE\sysgen\bin\nt64\libPortability.dll
