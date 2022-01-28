abfaacdf9a54116f7bc578b1411d8c5077637017
Cris <cbureriu@gmail.com>
2022-01-28 14:57:18 -0800

    Update revert.bat

--- revert.bat
+++ revert.bat
@@ -3,6 +3,7 @@ copy 14.7\ISE_DS\common\lib\nt\libPortability.dll.bak 14.7\ISE_DS\common\lib\nt\
 copy 14.7\ISE_DS\EDK\lib\nt\libPortability.dll.bak 14.7\ISE_DS\EDK\lib\nt\libPortability.dll
 copy 14.7\ISE_DS\ISE\lib\nt\libPortability.dll.bak 14.7\ISE_DS\ISE\lib\nt\libPortability.dll
 copy 14.7\ISE_DS\ISE\sysgen\bin\nt\libPortability.dll.bak 14.7\ISE_DS\ISE\sysgen\bin\nt\libPortability.dll
+copy 14.7\ISE_DS\ISE\lib\nt64\libPortability.dll.bak 14.7\ISE_DS\ISE\lib\nt64\libPortability.dll
 
 copy 14.7\ISE_DS\.xinstall\bin\nt64\libPortability.dll.bak 14.7\ISE_DS\.xinstall\bin\nt64\libPortability.dll
 copy 14.7\ISE_DS\common\lib\nt64\libPortability.dll.bak 14.7\ISE_DS\common\lib\nt64\libPortability.dll
