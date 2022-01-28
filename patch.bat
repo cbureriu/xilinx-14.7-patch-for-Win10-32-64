9834c4e292d28f045df9d1d0c5e038c112d3fbdc
Cris <cbureriu@gmail.com>
2022-01-28 14:56:47 -0800

    Update patch.bat

--- patch.bat
+++ patch.bat
@@ -3,7 +3,7 @@ copy 14.7\ISE_DS\common\lib\nt\libPortability.dll 14.7\ISE_DS\common\lib\nt\libP
 copy 14.7\ISE_DS\EDK\lib\nt\libPortability.dll 14.7\ISE_DS\EDK\lib\nt\libPortability.dll.bak
 copy 14.7\ISE_DS\ISE\lib\nt\libPortability.dll 14.7\ISE_DS\ISE\lib\nt\libPortability.dll.bak
 copy 14.7\ISE_DS\ISE\sysgen\bin\nt\libPortability.dll 14.7\ISE_DS\ISE\sysgen\bin\nt\libPortability.dll.bak
-
+copy 14.7\ISE_DS\ISE\lib\nt64\libPortability.dll 14.7\ISE_DS\ISE\lib\nt64\libPortability.dll.bak
 copy 14.7\ISE_DS\.xinstall\bin\nt64\libPortability.dll 14.7\ISE_DS\.xinstall\bin\nt64\libPortability.dll.bak
 copy 14.7\ISE_DS\common\lib\nt64\libPortability.dll 14.7\ISE_DS\common\lib\nt64\libPortability.dll.bak
 copy 14.7\ISE_DS\EDK\lib\nt64\libPortability.dll 14.7\ISE_DS\EDK\lib\nt64\libPortability.dll.bak
@@ -21,7 +21,4 @@ copy 14.7\ISE_DS\ISE\lib\nt64\libPortabilityNOSH.dll 14.7\ISE_DS\common\lib\nt64
 copy 14.7\ISE_DS\ISE\lib\nt64\libPortabilityNOSH.dll 14.7\ISE_DS\EDK\lib\nt64\libPortability.dll
 copy 14.7\ISE_DS\ISE\lib\nt64\libPortabilityNOSH.dll 14.7\ISE_DS\EDK\lib\nt64\sdk\libPortability.dll
 copy 14.7\ISE_DS\ISE\lib\nt64\libPortabilityNOSH.dll 14.7\ISE_DS\ISE\sysgen\bin\nt64\libPortability.dll
-
-
-
-
+copy 14.7\ISE_DS\ISE\lib\nt64\libPortabilityNOSH.dll 14.7\ISE_DS\ISE\lib\nt64\libPortability.dll
